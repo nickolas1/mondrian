46fa622bd97ea0f8f419ca60470a0c9f08abc133
Nick <nick@Nickolass-iMac.domain>
2019-10-13 09:51:54 -0500

    deploy

---
+++ precache-manifest.50ba6be57c1c2351cefbf04d5b5fb625.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "65c61b5f9d5b9ea37141",
+    "url": "/mondrian/css/app.796ac7cb.css"
+  },
+  {
+    "revision": "65c61b5f9d5b9ea37141",
+    "url": "/mondrian/js/app.dca14d45.js"
+  },
+  {
+    "revision": "72f78d39e4a99be2523a",
+    "url": "/mondrian/css/chunk-vendors.4388d46a.css"
+  },
+  {
+    "revision": "72f78d39e4a99be2523a",
+    "url": "/mondrian/js/chunk-vendors.ce405cf4.js"
+  },
+  {
+    "revision": "fa7eb3dddd7988c13069ecd996d54620",
+    "url": "/mondrian/index.html"
+  },
+  {
+    "revision": "59e98b1e448698614acfd65594c31d19",
+    "url": "/mondrian/android-chrome-192x192.png"
+  },
+  {
+    "revision": "316624ddeb0ce120311a7d9b9e6215ae",
+    "url": "/mondrian/android-chrome-512x512.png"
+  },
+  {
+    "revision": "05f1670970636df0c22a67c94be5b876",
+    "url": "/mondrian/apple-touch-icon.png"
+  },
+  {
+    "revision": "a5f7f2a4c93e6b2439b522d6c9ae5ee7",
+    "url": "/mondrian/favicon-16x16.png"
+  },
+  {
+    "revision": "15df5df96b253779a841b1ed4847e060",
+    "url": "/mondrian/favicon-32x32.png"
+  },
+  {
+    "revision": "061f63e8fbf1d2fe82cd8bf4d46f0a0c",
+    "url": "/mondrian/browserconfig.xml"
+  },
+  {
+    "revision": "eb69ff67b6f36deeccf8af9fda7e76ae",
+    "url": "/mondrian/img/example.png"
+  },
+  {
+    "revision": "de2c63f1f0f46ca7f807fe4af6f2d087",
+    "url": "/mondrian/mstile-150x150.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mondrian/robots.txt"
+  },
+  {
+    "revision": "ed97e731eac3b7317d53f9e675785983",
+    "url": "/mondrian/img/textile.png"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "/mondrian/site.webmanifest"
+  },
+  {
+    "revision": "411796d2b6f3907884b55eff779e43fc",
+    "url": "/mondrian/safari-pinned-tab.svg"
+  }
+];
